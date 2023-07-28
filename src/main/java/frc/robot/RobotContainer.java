e50db8856a46f466935bb1993154d9516675ea8c
LucaHaverty <lucahaverty@gmail.com>
2023-07-27 22:05:15 -0700

    Test commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,28 +4,20 @@
 
 package frc.robot;
 
-import com.pigmice.frc.lib.pathfinder.Pathfinder;
-import com.pigmice.frc.lib.shuffleboard_helper.ShuffleboardHelper;
+import java.util.HashMap;
+
 import com.pigmice.frc.lib.swerve.SwerveDrivetrain;
 import com.pigmice.frc.lib.swerve.commands.DriveWithJoysticks;
 import com.pigmice.frc.lib.swerve.commands.path_following.FollowPath;
-import com.pigmice.frc.lib.swerve.commands.path_following.RetracePath;
-import com.pigmice.frc.lib.swerve.commands.pathfinder.PathfindToPoint;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.DrivetrainConfig;
-import frc.robot.Constants.ShuffleboardTabs;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -41,7 +33,8 @@ public class RobotContainer {
     private XboxController driver = new XboxController(0);
     private XboxController operator = new XboxController(1);
     private Controls controls = new Controls(driver, operator);
-    private Pathfinder pathfinder = new Pathfinder(Units.inchesToMeters(30), "testing2");
+    // private Pathfinder pathfinder = new Pathfinder(Units.inchesToMeters(30),
+    // "testing2");
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -52,13 +45,17 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(new DriveWithJoysticks(drivetrain, controls::getDriveSpeedX,
                 controls::getDriveSpeedY, controls::getTurnSpeed, () -> true));
 
-        SmartDashboard.putNumber("Goal X", 0);
-        SmartDashboard.putNumber("Goal Y", 0);
-        ShuffleboardHelper.addOutput("GoalDriveable", ShuffleboardTabs.DRIVETRAIN,
-                () -> pathfinder.grid.FindCloseNode(new Translation2d(SmartDashboard.getNumber("Goal X", 0),
-                        SmartDashboard.getNumber("Goal Y", 0))).driveable);
-        ShuffleboardHelper.addOutput("CurrentDriveable", ShuffleboardTabs.DRIVETRAIN,
-                () -> pathfinder.grid.FindCloseNode(drivetrain.getPose().getTranslation()).driveable);
+        /*
+         * SmartDashboard.putNumber("Goal X", 0);
+         * SmartDashboard.putNumber("Goal Y", 0);
+         * ShuffleboardHelper.addOutput("GoalDriveable", ShuffleboardTabs.DRIVETRAIN,
+         * () -> pathfinder.grid.FindCloseNode(new
+         * Translation2d(SmartDashboard.getNumber("Goal X", 0),
+         * SmartDashboard.getNumber("Goal Y", 0))).driveable);
+         * ShuffleboardHelper.addOutput("CurrentDriveable", ShuffleboardTabs.DRIVETRAIN,
+         * () -> pathfinder.grid.FindCloseNode(drivetrain.getPose().getTranslation()).
+         * driveable);
+         */
     }
 
     /**
@@ -69,20 +66,24 @@ public class RobotContainer {
      * it to a {@link
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
-    private PathfindToPoint pathfindCommand;
+    // private PathfindToPoint pathfindCommand;
 
     private void configureButtonBindings() {
+
         new JoystickButton(driver, Button.kX.value).onTrue(new InstantCommand(() -> drivetrain.resetOdometry()));
-
-        new JoystickButton(driver, Button.kB.value).whileTrue(new RetracePath(drivetrain));
-
-        new JoystickButton(driver, Button.kA.value).onTrue(new InstantCommand(
-                () -> {
-                    pathfindCommand = new PathfindToPoint(drivetrain, pathfinder,
-                            new Pose2d(SmartDashboard.getNumber("Goal X", 0), SmartDashboard.getNumber("Goal Y", 0),
-                                    new Rotation2d()));
-                    pathfindCommand.schedule();
-                })).onFalse(new InstantCommand(() -> pathfindCommand.end(true)));
+        /*
+         * new JoystickButton(driver, Button.kB.value).whileTrue(new
+         * RetracePath(drivetrain));
+         * 
+         * new JoystickButton(driver, Button.kA.value).onTrue(new InstantCommand(
+         * () -> {
+         * pathfindCommand = new PathfindToPoint(drivetrain, pathfinder,
+         * new Pose2d(SmartDashboard.getNumber("Goal X", 0),
+         * SmartDashboard.getNumber("Goal Y", 0),
+         * new Rotation2d()));
+         * pathfindCommand.schedule();
+         * })).onFalse(new InstantCommand(() -> pathfindCommand.end(true)));
+         */
     }
 
     public void stopControllerRumble() {
@@ -96,6 +97,8 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return new FollowPath(drivetrain, "New New Path", false);
+        var eventMap = new HashMap<String, Command>();
+        eventMap.put("TestEvent", new InstantCommand(() -> System.out.println("EVENT CALLED")));
+        return new FollowPath(drivetrain, "New New Path", eventMap, false);
     }
 }
